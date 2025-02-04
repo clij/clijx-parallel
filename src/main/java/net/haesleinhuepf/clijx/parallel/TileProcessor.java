e754cd6e0f294b670641576464c09e8e76e643f9
Nicolas Chiaruttini <nicolas.chiaruttini@epfl.ch>
2025-02-04 16:50:55 +0100

    Fix javadoc error

--- src/main/java/net/haesleinhuepf/clijx/parallel/TileProcessor.java
+++ src/main/java/net/haesleinhuepf/clijx/parallel/TileProcessor.java
@@ -8,8 +8,8 @@ import java.util.function.BiConsumer;
 /**
  * Note: Those workflows should not result in label images as identical labels can exist in neighboring tiles.
  * Workflows should result in filtered intensity images, binary images or parametric maps only.
- *
- * @author: Robert Haase
+ * <p>
+ * author: Robert Haase
  */
 public interface TileProcessor extends BiConsumer<ClearCLBuffer, ClearCLBuffer> {
     void setCLIJx(CLIJx clijx);
